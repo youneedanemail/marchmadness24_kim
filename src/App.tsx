4508662b63e067878142b66deb96a94bd9c8c9b7
youneedanemail <youneedanemail.com>
2024-03-08 18:38:56 -0700

    added welcome & intro fuction imported data

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
 // Kimberly March Madness Project 2024
 
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import data from "./CollegeBasketballTeams.json";
+
+// Header
+function Welcome() {
+  return <h1>Welcome To March Madness 2024</h1>;
+}
+
+// Info/Description
+function Intro() {
+  return (
+    <div>
+      <h3>
+        Join us for the madness of basketball this year <br />
+        as we watch teams go head-to-head for the this year's championship win.
+      </h3>
+      <p>Check out the teams below:</p>
+    </div>
+  );
+}
+
+// Interface
+
+// Bring in json
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome />
+      <Intro />
     </div>
   );
 }
